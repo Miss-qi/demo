0c04026028df9074e986a34777af8c993ca374a9
Miss-qi <1370322806@qq.com>
2016-08-04 22:12:42 +0800

    get parameter by query

--- spec/server_spec.js
+++ spec/server_spec.js
@@ -1,3 +1,22 @@
 /**
  * Created by qiyanzi on 16-8-4.
  */
+'use strict';
+const supertest = require('supertest');
+const app = require('../src/server');
+const request = supertest(app);
+describe('get /query', function () {
+    it('should get parameter by query', function (done) {
+        request
+            .get('/query')
+            .query({name:'Tom'})
+            .expect('Tom')
+            .end((err, res) => {
+                if (err) {
+                    done.fail(err);
+                } else {
+                    done();
+                }
+            });
+    });
+});
--- src/server.js
+++ src/server.js
@@ -1,3 +1,18 @@
 /**
  * Created by qiyanzi on 16-8-4.
  */
+const express = require('express');
+let app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extend: true}));
+app.use(bodyParser.json());
+
+app.get('/query',(req,res)=>{
+    res.send(req.query.name);
+});
+
+app.listen(3000,()=>{
+    console.log('Example app listening on port 3000!');
+});
+
+module.exports = app;
